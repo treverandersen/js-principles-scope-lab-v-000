b740ae85da1949b64b617c213d32b99306411f7d
Trever Andersen <trever.r.andersen@gmail.com>
2018-01-23 18:05:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 var customerName = 'bob';
 
 function upperCaseCustomerName() {
-  let customerName = customerName.toUpperCase()
-  return customerName
+  var customerName = customerName.toUpperCase()
 }
 
 function setBestCustomer() {
