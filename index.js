724fac115c6c0cf948a6b7d556e5be1fa0c31422
Trever Andersen <trever.r.andersen@gmail.com>
2018-01-23 17:56:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  let customerName = customerName.toUpperCase()
+  return customerName
+}
+
+function setBestCustomer() {
+  var bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+
+}
